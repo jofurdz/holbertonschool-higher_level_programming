b28f5e0c0f20d72d77e71d402a6aa83d67d3a9c4
jofurdz <jofurdz@gmail.com>
2021-09-29 12:22:46 -0500

    task 1

---
+++ 0x01-python-import_modules/1-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+argc = len(sys.argv) - 1
+
+if argc == 0:
+    print("{:d} arguments.".format(0, sys.argv[argc]))
+elif argc == 1:
+    print("{:d} arguments:".format(1))
+else:
+    print("{:d} arguments:".format(argc))
+
+for x in range(1, argc + 1):
+    print("{:d}: {}".format(x, sys.argv[x]))
