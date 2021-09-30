aa709083ab425a78537c2ba12bd2e0e659493c24
jofurdz <jofurdz@gmail.com>
2021-09-30 17:22:43 -0500

    task 10

---
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = list(my_list)
+    for x in my_list:
+        if x % 2 == 0:
+            new_list[x] = True
+        else:
+            new_list[x] = False
+        return new_list
