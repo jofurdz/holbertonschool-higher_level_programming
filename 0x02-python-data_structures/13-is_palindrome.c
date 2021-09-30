4edcbb29f44eab7220b3a37a17d0c110834de7d2
jofurdz <jofurdz@gmail.com>
2021-09-30 17:59:51 -0500

    s;lKDJGFLsdkjgl;kdsj

---
+++ 0x02-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks for palindrome
+ *
+ * @head: head
+ */
+int is_palindrome(listint_t **head)
+{
+if (head == NULL)
+{
+	return (0);
+}
+return (1);
+}
