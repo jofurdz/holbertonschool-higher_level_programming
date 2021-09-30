366dd683383375ba7185af7b5c9974f42a24cc87
jofurdz <jofurdz@gmail.com>
2021-09-30 17:12:01 -0500

    task 9

---
+++ 0x02-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    store = 0
+    if my_list:
+        for x in my_list:
+            if x > store:
+                store = x
+    else:
+        return None
+    return store
