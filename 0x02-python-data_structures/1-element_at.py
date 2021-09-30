f3f5f84764a2c6ce4654a6e6203f218f29e48dbc
jofurdz <jofurdz@gmail.com>
2021-09-30 13:44:46 -0500

    edge case for 1

--- 0x02-python-data_structures/1-element_at.py
+++ 0x02-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    length = len(my_list)
-    if idx < 0 and idx >= length:
+    if idx < 0 and idx >= len(my_list):
         return None
     else:
         return my_list[idx]
