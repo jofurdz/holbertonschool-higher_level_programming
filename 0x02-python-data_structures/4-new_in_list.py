e1a6e198def3c957845b1a7f8ddda6e9903dc28a
jofurdz <jofurdz@gmail.com>
2021-09-30 14:07:41 -0500

    task 4

--- 0x02-python-data_structures/4-new_in_list.py
+++ 0x02-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     newList = my_list.copy()
-    if idx < 0:
+    if idx < -1:
         return newList
     elif idx > len(newList):
         return my_list
