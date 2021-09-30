802c835cae3d1453ee040d665cd240615d8949f2
jofurdz <jofurdz@gmail.com>
2021-09-30 14:11:15 -0500

    i hate the non explicit checker

--- 0x02-python-data_structures/4-new_in_list.py
+++ 0x02-python-data_structures/4-new_in_list.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    newList = my_list.copy()
-    if idx < -1:
-        return newList
-    elif idx > len(newList):
-        return my_list
-    else:
-        newList[idx] - element
-        return newList
+    newList = my_list[:]
+    if idx > -1 and idx < len(newList):
+        newList[idx] = element
+    return newList
