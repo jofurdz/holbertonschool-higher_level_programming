59394f2dd52c1f6b2ef3d420a5257cd927669648
jofurdz <jofurdz@gmail.com>
2021-10-01 10:10:17 -0500

    edge case for 10

--- 0x02-python-data_structures/10-divisible_by_2.py
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -2,10 +2,10 @@
 def divisible_by_2(my_list=[]):
     if not my_list:
         return my_list
-    new_list = list(my_list)
+    new_list=[]
     for x in my_list:
         if x % 2 == 0:
-            new_list[x] = True
+            new_list.append(True)
         else:
-            new_list[x] = False
+            new_list.append(False)
     return new_list
