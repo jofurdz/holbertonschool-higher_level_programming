d489a145a8bab023aecc5c6728e3f99c6e6e3acb
jofurdz <jofurdz@gmail.com>
2021-10-01 10:11:22 -0500

    pepe silvia

--- 0x02-python-data_structures/10-divisible_by_2.py
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     if not my_list:
         return my_list
-    new_list=[]
+    new_list = []
     for x in my_list:
         if x % 2 == 0:
             new_list.append(True)
