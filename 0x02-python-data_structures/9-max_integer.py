17356d4c9502e8061aca39ddfb322d4a48f7d767
jofurdz <jofurdz@gmail.com>
2021-10-01 09:39:32 -0500

    edge case for task 9

--- 0x02-python-data_structures/9-max_integer.py
+++ 0x02-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    store = my_list[0]
     if my_list:
+        store = my_list[0]
         for x in my_list:
             if x > store:
                 store = x
