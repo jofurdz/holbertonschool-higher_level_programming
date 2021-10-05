e7372c0747cfd6ad4fc8b4821f47b3a848ab97e3
jofurdz <jofurdz@gmail.com>
2021-10-05 14:24:06 -0500

    task 3

---
+++ 0x04-python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Defines a class Square by size"""
+
+
+class Square:
+    """ Defines an object size """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
