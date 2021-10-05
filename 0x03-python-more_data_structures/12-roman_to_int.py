f182d1bc55d5dd338bc45904eca915eb459e1014
jofurdz <jofurdz@gmail.com>
2021-10-04 22:18:25 -0500

    ;lfkdg;lfkg;l

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,8 @@
 def roman_to_int(roman_string):
     rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     int_val = 0
+    if type(roman_string) != str or roman_string is None:
+        return 0
     for i in range(len(roman_string)):
         if i > 0 and rom_val[roman_string[i]] > rom_val[roman_string[i - 1]]:
             int_val += rom_val[roman_string[i]] - 2 * \
