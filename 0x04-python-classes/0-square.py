1e22c6032254abc83b2eef390dae5bc1e34b39f7
jofurdz <jofurdz@gmail.com>
2021-10-05 12:42:52 -0500

    task 0 and README

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Defines a class Square by size """
+
+
+class Square:
+    """ Defines an object size without verification """
+    def __init__(self, size):
+        self.__size = size
