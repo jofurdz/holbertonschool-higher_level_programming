4ca22aec63e8b3b378ad78863bc54e1db905a827
jofurdz <jofurdz@gmail.com>
2021-10-07 22:10:19 -0500

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for x in str:
+        x = ord(x)
+        if x >= 97 and x <= 122:
+            x -= 32
+        print("{:c}".format(x), end="")
+    print()
