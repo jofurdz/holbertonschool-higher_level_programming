797e92e47d6f0be81f723d562fe3029360367693
jofurdz <jofurdz@gmail.com>
2021-10-11 16:27:27 -0500

    dskjhgfkdjhgfkljdsh

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-""" say_my_name prints first_name followed by last_name """
+"""prints first_name followed by last_name"""
 
 
 def say_my_name(first_name, last_name=""):
-    if type(first_name) != str:
+    """prints string of first and last name"""
+
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
 
-    if type(last_name) != str:
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
 
-    print("My name is {:s} {:s}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,3 +1,16 @@
-#!/usr/python3
+#!/usr/bin/python3
+"""prints a square using #"""
+
+
 def print_square(size):
-    
+    """prints a square using #"""
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise TypeError("size must be >= 0")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    for x in range(size):
+        for y in range(size):
+            print("#", end='')
+        print()
