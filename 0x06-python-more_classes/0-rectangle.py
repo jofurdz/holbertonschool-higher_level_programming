07eccd498da5ac22050e6b09c89aa07bfcb37920
jofurdz <jofurdz@gmail.com>
2021-10-12 11:00:13 -0500

    pep8

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -4,7 +4,7 @@
 
 class Rectangle:
     """class rectangle"""
-    
+
     def __init__(self, width=0, height=0):
         if type(width) is not int:
             raise TypeError("width must be an integer")
