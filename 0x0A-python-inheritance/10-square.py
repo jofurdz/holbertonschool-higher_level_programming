7e9dcaf3043792df383efb4e548f79695e47b330
jofurdz <jofurdz@gmail.com>
2021-10-13 19:18:38 -0500

    oopsie task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""subclass Square that inherits from Rectangle"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """subclass Square that inherits from Rectangle"""
+    def __init__(self, size):
+        """init for square"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """area of Square"""
+        return self.__size * self.__size
