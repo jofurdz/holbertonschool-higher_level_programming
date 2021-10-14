a44c0dc386a784a4cfa3518b6ac2c7ad41afa3a5
jofurdz <jofurdz@gmail.com>
2021-10-14 15:39:26 -0500

    added documentation

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""creates class that inherits"""
+
 
 class MyList(list):
-
+    """class that inherits"""
     def __init__(self):
         super().__init__()
-    
+
     def print_sorted(self):
         print(sorted(self))
