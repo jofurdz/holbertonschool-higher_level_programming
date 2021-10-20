049c265b4aeb2829d8b1fe67cce5ebcbff5a21e5
jofurdz <jofurdz@gmail.com>
2021-10-20 11:19:38 -0500

    tasks 3 - 7

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,7 +11,17 @@ class Base:
         if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-        else
+        else:
             self.id = self
 
-    
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+
+    def xyValidator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value < 0:
+            raise ValueError("{} must be >= 0".format(name))
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,10 +4,79 @@
 from models.base import Base
 
 
-class Rectangle(base):
+class Rectangle(Base):
     """class Rectangle that inherits from Base"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
 
-        
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+        self.xyValidator("x", x)
+        self.__x = x
+        self.xyValidator("y", y)
+        self.__y = y
+
+    def area(self):
+        """returns area"""
+        return self.__height * self.__width
+
+    def display(self):
+        """prints in stdout the Rectangle instance with the character #"""
+        for y in range(self.__y):
+            print()
+        for h in range(self.__height):
+            for w in range(self.__width):
+                if w == 0:
+                    for x in range(self.__x):
+                        print(" ", end="")
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """returns rectangle"""
+        i = self.id
+        x = self.__x
+        y = self.__y
+        w = self.__width
+        h = self.__height
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(i, x, y, w, h))
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        self.integer_validator("width", value)
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.integer_validator("height", value)
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.xyValidator("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.xyValidator("y", value)
+        self.__y = value
+
