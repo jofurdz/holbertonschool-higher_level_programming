4a274b5337c99e4e9c25a5b2596661ea95875c5c
jofurdz <jofurdz@gmail.com>
2021-10-21 11:02:26 -0500

    task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,3 +15,31 @@ class Square(Rectangle):
         y = self.y
         w = self.width
         return "[Square] ({}) {}/{} - {}".format(i, x, y, w)
+
+    def update(self, *args, **kwargs):
+        """assigns attributes to arguments"""
+        attributes = ["id", "size", "x", "y"]
+        for j in range(len(args)):
+            if j == 0:
+                self.id = args[j]
+            if j == 1:
+                self.size = args[j]
+            if j == 2:
+                self.x = args[j]
+            if j == 3:
+                self.y = args[j]
+
+        if not args:
+            for key, value in kwargs.items():
+                self.__setattr__(key, value)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+
