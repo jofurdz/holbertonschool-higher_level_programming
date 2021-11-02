5965acd6f3a19eaa03d42ca2bbf4fad44d348135
jofurdz <jofurdz@gmail.com>
2021-11-02 14:01:14 -0500

    task 9 and 10

---
+++ 0x00-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- lists all records of a table
+
+
+SELECT score, name FROM second_table ORDER BY score DESC, name;
---
+++ 0x00-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- creates a table and adds multiple rows
+
+
+CREATE TABLE if not EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score)
+VALUES (1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
