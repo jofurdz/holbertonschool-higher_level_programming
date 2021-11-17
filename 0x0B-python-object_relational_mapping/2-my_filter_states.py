98b006925cdb1944f2aaa22cc65b1fa8a3094063
jofurdz <jofurdz@gmail.com>
2021-11-17 10:35:07 -0600

    task 2

---
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""takes an argument and displays all values in the states table"""
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", port=3306, userName=argv[1],
+                         password=argv[2], dataBase=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT id, name FROM states WHERE BINARY name = '{}'\
+                ORDER BY id ASC".format(argv[4]))
+    poopla = cur.fetchall()
+    for x in poopla:
+        print(x)
+
+    cur.close()
+    conn.close()
