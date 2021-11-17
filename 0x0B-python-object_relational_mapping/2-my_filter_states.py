b12fe3b6f7efb1f50a12b7b435b4f2cccd2e4317
jofurdz <jofurdz@gmail.com>
2021-11-17 10:37:19 -0600

    task 2 update

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """takes an argument and displays all values in the states table"""
 if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
+
     db = MySQLdb.connect(host="localhost", port=3306, userName=argv[1],
                          password=argv[2], dataBase=argv[3])
     cur = conn.cursor()
