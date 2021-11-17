48afb4ec412458ed235823c9d4f30ed2dc3fd714
jofurdz <jofurdz@gmail.com>
2021-11-17 09:20:10 -0600

    task 1?

---
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,13 @@
+#!/ur/bin/python3
+"""lists all states with N from database"""
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    db = MySQLdb.connect(host="localhost", port=3306, userName=argv[1],
+                         password=argv[2], dataBase=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    poopla = cur.fetchall()
+    for x in poopla:
+        if x[1].startswith('N'):
+            print(x)
