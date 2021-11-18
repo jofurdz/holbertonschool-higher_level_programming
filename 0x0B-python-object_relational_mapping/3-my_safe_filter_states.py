dc558b5f824d61587dbd000f48d1f31affc27a92
jofurdz <jofurdz@gmail.com>
2021-11-17 19:10:01 -0600

    task 3

---
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""takes arguments and displays all values that match"""
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, userName=argv[1],
+                         password=argv[2], dataBase=argv[3])
+    cur = db.cursor()
+
+    cur.execute(
+        "SELECT * FROM states WHERE name = %(name)s\
+         ORDER BY states.id ASC", {'name': argv[4]})
+    poopla = cur.fetchall()
+    for x in poopla:
+        print(x)
+    cur.close
+    db.close()
