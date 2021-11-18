54ca2604c82dd47c4009f23bfa608c01b63130b7
jofurdz <jofurdz@gmail.com>
2021-11-17 20:43:13 -0600

    update

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -3,8 +3,8 @@
 if __name__ == "__main__":
     from sys import argv
     import MySQLdb
-    db = MySQLdb.connect(host="localhost", port=3306, userName=argv[1],
-                         password=argv[2], dataBase=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     poopla = cur.fetchall()
