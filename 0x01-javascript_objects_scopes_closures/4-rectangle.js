68a90da757d9af68856f611598c763f906911e0f
jofurdz <jofurdz@gmail.com>
2022-01-07 13:06:05 -0600

    task 4

---
+++ 0x01-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    for (let x = 0; x < this.height; x++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
